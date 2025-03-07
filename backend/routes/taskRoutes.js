d82724b202e32915dc19af264a05ef02a081aaf1
Ahmad <111832798+Aboomar524@users.noreply.github.com>
2025-03-07 11:24:24 -0500

    Update taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Task = require("../models/task");
+const Task = require("../models/tasks");
 
 // Create Task
 router.post("/tasks", async (req, res) => {
