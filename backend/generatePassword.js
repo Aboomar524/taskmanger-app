861c9a5579b0107446c7aa755f052aeb612bb09a
Ahmad <111832798+Aboomar524@users.noreply.github.com>
2025-04-02 02:16:38 -0400

    a

--- backend/generatePassword.js
+++ backend/generatePassword.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 
-const password = 'LetMeIn!'; // كلمة المرور التي تريد تشفيرها
+const password = 'Ahmad5!'; // كلمة المرور التي تريد تشفيرها
 const salt = bcrypt.genSaltSync(10); // توليد الملح
 const hashedPassword = bcrypt.hashSync(password, salt); // تشفير كلمة المرور
 
