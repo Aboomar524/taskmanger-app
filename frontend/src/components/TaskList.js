06333bb6472f2df39479039167cccdf60d6b9a51
Ahmad <111832798+Aboomar524@users.noreply.github.com>
2025-04-23 23:44:54 -0400

    Update TaskList.js

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api/tasks";
 
-const API_URL = "http://localhost:5000/api/tasks";
 
 
 const TaskList = () => {
